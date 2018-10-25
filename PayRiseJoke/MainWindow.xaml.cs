95e871be302b1412d8b167e7c385ae74cc7bc758
Michał Krause <michal.krause01@gmail.com>
2018-10-25 15:38:00 +0200

    Added functionality for buttons changing places
    -Buttons change places when user enters mouse pointer on YES button
    
    -Also added label that shows that you clicked NO button instead of YES

--- PayRiseJoke/MainWindow.xaml.cs
+++ PayRiseJoke/MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace PayRiseJoke
     /// </summary>
     public partial class MainWindow : Window
     {
+        
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void btnYes_MouseEnter(object sender, MouseEventArgs e)
+        {
+            var tempMarginLeft = btnNo.Margin.Left;
+            var tempMarginTop = btnNo.Margin.Top;
+            var tempMarginRight = btnNo.Margin.Right;
+            var tempMarginBottom = btnNo.Margin.Bottom;
+            var tempHorizontalAlignment = btnNo.HorizontalAlignment;
+
+            btnNo.HorizontalAlignment = btnYes.HorizontalAlignment;
+            btnNo.Margin = new Thickness(btnYes.Margin.Left, btnYes.Margin.Top, btnYes.Margin.Right,
+                btnYes.Margin.Bottom);
+
+            btnYes.HorizontalAlignment = tempHorizontalAlignment;
+            btnYes.Margin = new Thickness(tempMarginLeft, tempMarginTop, tempMarginRight, tempMarginBottom);
+        }
+
+        private void btnNo_Click(object sender, RoutedEventArgs e)
+        {
+            lblAnswer.Content = "Your answer is NO";
+        }
+
+       
+
     }
 }
